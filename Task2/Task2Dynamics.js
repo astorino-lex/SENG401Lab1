958e0b328001c1a58f6d189c8f36f6905f1f73df
Alexa Astorino <alexa.astorino@gmail.com>
2019-01-21 18:48:34 -0700

    separated into task folders, added task 2 code

---
+++ Task2/Task2Dynamics.js
@@ -0,0 +1,39 @@
+function mouseOverText(x) {
+	x.style.color = "white";
+}
+
+function mouseOutText(x) {
+	x.style.color = "black";
+}
+
+function mouseOverOther(x) {
+	x.style.border = "2px solid white";
+}
+
+function mouseOutOther(x) {
+	x.style.border = "2px solid #94B5EF";
+}
+
+function mouseOverInput(x) {
+	x.style.border = "2px solid black"
+}
+
+function mouseOutInput(x) {
+	x.style.border = "2px solid #94B5EF";
+}
+
+function mouseOverCircle(x) {
+	x.src = "circleyellow.png";
+}
+
+function mouseOutCircle(x) {
+	x.src= "circle.png"
+}
+
+function mouseOverSection(x, y) {
+	x.querySelector("#toDisplay").innerHTML = y.id;
+}
+
+function mouseOutSection(x) {
+	x.querySelector("#toDisplay").innerHTML = "&nbsp;&nbsp;&nbsp;";
+}
